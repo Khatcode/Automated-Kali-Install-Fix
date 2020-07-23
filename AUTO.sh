c921b28faa1d24fea4c0f7db1070380d5bac6383
Khatcode <68708968+Khatcode@users.noreply.github.com>
2020-07-23 15:19:45 -0400

    Update AUTO.sh

--- AUTO.sh
+++ AUTO.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Make sure to run as root"
 echo "Auto Download For Kali Linux Script Because I'm lazy"
-git clone https://github.com/manves/kalifix
-cd kalifix
+git clone https://github.com/Khatcode/Kali-Linux-Source-Fix
+cd Kali-Linux-Source-Fix
 chmod +x kalifix.sh
 sudo ./kalifix.sh
