6c7ebfb6513bc73ff9015ce4d83de7a384441c4b
Khatcode <68708968+Khatcode@users.noreply.github.com>
2020-07-23 14:56:41 -0400

    Add files via upload

---
+++ AUTO.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Make sure to run as root"
+echo "Auto Download For Kali Linux Script Because I'm lazy"
+git clone https://github.com/manves/kalifix
+cd kalifix
+chmod +x kalifix.sh
+sudo ./kalifix.sh
